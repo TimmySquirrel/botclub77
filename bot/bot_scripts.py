7d424e62c1fa52426e8d7f1c9ac6e3cc090bd5f5
TimmySQvrL <89269750004@list.ru>
2024-09-15 18:56:07 +0300

    New function and checked

--- bot/bot_scripts.py
+++ bot/bot_scripts.py
@@ -37,7 +37,7 @@ def GetIndexLastNewLine(text: str):
     return a if a != -1 else 800
 
 # Получаем пост из ВК
-def GetMsgFromVK(token_vk):
+def GetMsgFromVK(token_vk: str):
     # открываем сессию  с ВК
     print("GetMsgFromVK start ...")
     vk = vk_api.VkApi(token=token_vk)
@@ -55,61 +55,70 @@ def GetMsgFromVK(token_vk):
 
         
 # Отправляем в телегу сообщение
-def SendMSG2Telegram(url, post_param, chat_id):
+def SendMSG2Telegram(url: str, post_param: dict, chat_id: int):
     print("SendMSG2Telegram start ...")
     if len(post_param['photo']) == 0:
         r = requests.post(url + '/sendMessage', data={"chat_id": chat_id,
-                                                      "text": post_param['text']})
+                                                      "text": post_param['text'] + post_param['link']})
                                                     #   "text": post_param['text'],
                                                     #   "disable_web_page_preview": True})
         PrintLog(r, 'SendMSG2Telegram', '/sendMessage')
     else:
-        text = post_param['text']
+        text = post_param['text'] + post_param['link']
         if len(post_param['text']) > 1024:
             a = GetIndexLastNewLine(post_param['text'])
-            text = post_param['text'][:a] + f"\n[Все не влезло, продолжение по ссылке {post_param['link']} ©MSGBot]"
-            post_param['text'] = ''
-            # text = post_param['text'][:a] + '\n[Все не влезло, перенес в комментарии...©MSGBot] '
+            # text = post_param['text'][:a] + f"\n[Все не влезло, продолжение по ссылке {post_param['link']} ©MSGBot]"
+            # post_param['text'] = ''
             # logger.info("Все не влезло, перенес в комментарии...")
-            # post_param['text'] = '[На чем мы тут остановились...©MSGBot]\n' + post_param['text'][a:]
+            text = post_param['text'][:a] + '\n[Все не влезло, перенес в комментарии...] '
+            post_param['text'] = '[На чем мы тут остановились...]\n' + post_param['text'][a:] + post_param['link']
         else:
             post_param['text'] = ''
         r = requests.post(url + "/sendPhoto", data={"chat_id": chat_id,
                                                     "photo": post_param['photo'],
                                                     "caption": text})
         PrintLog(r, 'SendMSG2Telegram', '/sendPhoto')
+        if r.status_code == 200:
+            post_param.update({'owner_chat_id': r.json()['result']['chat']['id'], 'owner_message_id': r.json()['result']['message_id']})
     return post_param if r.status_code == 200 else None
 
 
 # Закрепляем пост
-def pinChatMessage(url, chat_id, message_id):
+def pinChatMessage(url:str, chat_id:int, message_id:int):
     r = requests.post(url + '/pinChatMessage', data = {"chat_id": chat_id, "message_id": message_id})
     PrintLog(r, 'pinChatMessage', '/pinChatMessage')  
 
+# поиск последнего сообщения
+def GetChatAndMSGID(from_chat_id:int, from_msg_id:int):
+    data_json = requests.post(tg_url + '/getUpdates', data = {'offset': -15})
+    PrintLog(data_json, 'GetChatAndMSGID', '/getUpdates')  
+    if data_json.status_code == 200:
+        for result in data_json.json()['result']:
+            if (result.get('message')  
+                and result['message'].get('is_automatic_forward') 
+                and result['message']['forward_from_chat']['id'] == from_chat_id
+                and result['message']['forward_from_message_id'] == from_msg_id):
+                return {'update_id': result['update_id'], 'message_id': result['message']['message_id'], 'chat_id': result['message']['chat']['id']}
+
 # комметрируем пост, параметры поста получаем через getUpdates
-def MessageReplies(url, post_param):
+def MessageReplies(url:str, post_param:dict):
     print("MessageReplies start ...") 
     if post_param != None: 
-        r = requests.post(url + '/getUpdates')
-        PrintLog(r,"MessageReplies", "/getUpdates") 
-        if r.status_code == 200:
-            Dict = r.json()
-            Len_Dict = len(Dict['result'])-1
-            Last_Result = Dict['result'][Len_Dict]
-            Owner_Channel_ID = Last_Result['message']['chat']['id']
-            Reply_ID = Last_Result['message']['message_id'] 
+        ChatParam = GetChatAndMSGID(post_param['owner_chat_id'], post_param['owner_message_id'])
+        if ChatParam:
             if post_param['text'] != '':
                 # logger.info("post_param['text'] != ''")
-                r = requests.post(url + '/sendMessage', data={"chat_id": Owner_Channel_ID, 
-                                                            "reply_to_message_id": Reply_ID, 
+                r = requests.post(url + '/sendMessage', data={"chat_id": ChatParam['chat_id'], 
+                                                            "reply_to_message_id": ChatParam['message_id'], 
                                                             "text": post_param['text']}) 
                 PrintLog(r,"MessageReplies", "/sendMessage") 
-                # делаем закреп
-                pinChatMessage(url, r.json()['result']['chat']['id'], r.json()['result']['message_id'])
+                if r.status_code == 200:
+                    # делаем закреп
+                    pinChatMessage(url, r.json()['result']['chat']['id'], r.json()['result']['message_id'])
             if post_param['poll'] != '': 
                 # logger.info("post_param['poll'] != ''")
-                r = requests.post(url + '/sendPoll', data={"chat_id": Owner_Channel_ID, 
-                                                        "reply_to_message_id": Reply_ID, 
+                r = requests.post(url + '/sendPoll', data={"chat_id": ChatParam['chat_id'], 
+                                                        "reply_to_message_id": ChatParam['message_id'], 
                                                         "question": post_param['poll'][0], 
                                                         "options": json.dumps(post_param['poll'][1]), 
                                                         "is_anonymous": False})  
@@ -124,8 +133,8 @@ if __name__ == '__main__':
             print("Begin ...")
             text = GetMsgFromVK(token_vk)
             AnswerTG = SendMSG2Telegram(tg_url, text, channel_id)
-            # time.sleep(5)
-            # MessageReplies(tg_url, AnswerTG)
+            time.sleep(5)
+            MessageReplies(tg_url, AnswerTG)
             print("End.")
         except Exception:
             # logger.warning("Переподключение")
--- bot/main.py
+++ bot/main.py
@@ -1,19 +1,66 @@
-import time 
+import time, codecs 
 from bot_scripts import *
 from bot_config import token_vk, channel_id
 
-if __name__ == '__main__':
-    while True:
-        try:
-            AnswerTG = SendMSG2Telegram(tg_url, GetMsgFromVK(token_vk), channel_id)
-            time.sleep(5)
-            MessageReplies(tg_url, AnswerTG)
-        except Exception:
-            # logger.warning("Переподключение")
-            time.sleep(10)
-            pass
+# if __name__ == '__main__':
+#     while True:
+#         try:
+#             AnswerTG = SendMSG2Telegram(tg_url, GetMsgFromVK(token_vk), channel_id)
+#             time.sleep(5)
+#             MessageReplies(tg_url, AnswerTG)
+#         except Exception:
+#             # logger.warning("Переподключение")
+#             time.sleep(10)
+#             pass
 
-    
+
+
+
+
+r = requests.post(tg_url + '/getUpdates') 
+                                                # 'offset': -10, '
+json_file = r.json()
+print(GetChatAndMSGID(json_file, -1002162249193, 146))
+# print(json.dumps(json_file['result'][0],  indent=4))
+# for i,result in enumerate(json_file['result']):
+#     if result.get('message') and result['message'].get('is_automatic_forward') and result['message']['forward_from_message_id'] == 146:
+#         # print(result)
+#         print(i, result['message']['message_id'], result['message']['chat']['id'])
+    # if result.get('message') and result['message'].get('sender_chat'):
+    #     print(i, result['message'])
+        # with open('file/data_update_botresult.json', 'w') as file:
+        #     json.dump(result, file, indent=4)
+    # if result.get('message') and result['message'].get('sender_chat'):
+    #     print(i, result['message'])
+
+# # # with open('file/data_update.json', 'w') as file:
+# # #     file.read(r.text)
+
+# with open('file/data_update.json', 'w') as file:
+#     json.dump(json.loads(r.text), file, indent=4)
+# print(json.dump(json.loads(r.text), ensure_ascii = False,  indent=4))
+# print(json.dumps(r.text,  indent=4))
+
+# 
+#     # json.dump(r.text, file, indent=4)
+# with open('file/data_update.json') as file:
+#     json_file = json.load(file)
+# # #     # print(json_file["result"][0])
+#     for result in json_file["result"]:
+        
+#         if result.get('message')  and  result['message']['chat']['id'] == -1002430633954: 
+#             # and result['message'].get('sender_chat')
+#             print(result['message'],'\n')
+        # print(json.dumps(result,  indent=4)+'\n')
+        # if result.get('channel_post'):
+        #     print(result['channel_post'])
+        # if result.get('message') and result['message']['from']['is_bot']:
+        #     # print(result['message']['chat']['id'], result['message']['message_id'])
+        #    print(result['message'])
+        
+# print(codecs.decode(r.text,'unicode_escape'))
+# pinChatMessage(tg_url, -1001195655967)
+
     
 
 
